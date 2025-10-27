c37ad363ab167292870140e0d44f1377628e3006
AkiStory <sonsungjeon@gmail.com>
2025-10-27 12:31:32 +0900

    2025.10.27

--- src/main/java/com/kt/controller/UserController.java
+++ src/main/java/com/kt/controller/UserController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kt.dto.UserCreateRequest;
+
 @RestController
 public class UserController {
 	@PostMapping("users")
 	@ResponseStatus(HttpStatus.CREATED)
-	//loginID, password, name, birthday
-	public void create(@RequestBody User user) {
+	// loginID, password, name, birthday
+	// json형태의 body에 담겨서 post요청으로 /users 가 들어오면
+	// @RequestBody를 보고 jacksonObjectMapper가 동작해서 json을 읽어서 dto로 변환
+	public void create(@RequestBody UserCreateRequest request) {
+		// jackson object mapper -> json to dto를 매핑
 	}
 }
