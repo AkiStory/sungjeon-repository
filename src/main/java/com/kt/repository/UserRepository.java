f7c5f82d4b7af8b0cd478ee19b07a5b818723b51
AkiStory <sonsungjeon@gmail.com>
2025-11-06 13:01:31 +0900

    restart issue

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -1,21 +1,29 @@
 package com.kt.repository;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.kt.domain.User;
+import com.kt.domain.user.User;
 
-import lombok.RequiredArgsConstructor;
+// <T, ID>
+// T: Entity 클래스 => User
+// ID: Entity 클래스의 PK 타입 => Long
+public interface UserRepository extends JpaRepository<User, Long> {
+	//여기에 쿼리를 작성해줘야
+	// JPA에서는 쿼리를 작성하는 3가지 방법이 존재
+	// 1. 네이티브 쿼리 작성 (3)
+	// 2. JPQL 작성 -> 네이티브 쿼리랑 같은데 Entity 기반 (2) - 너무길어진 메소드이름을 그냥 쿼리작성해서 숨김
+	// 3. querymethod 작성 -> 메서드 이름을 쿼리 처럼 작성 (1) - 길어지면 상당히 이상해보임
+	// 찾는다 : findByXX , 존재하냐? existsByXX, 삭제 : deleteByXX
 
-@Repository
-@RequiredArgsConstructor
-public class UserRepository {
-	private final JdbcTemplate jdbcTemplate;
+	Boolean existsByLoginId(String loginId);
 
-	public void save(User user) {
-		// 서비스에서 dto를 도메인(비지니스모델)으로 바꾼다음 전달
-		var sql = "INSERT INTO MEMBER (loginId, password, name, birthday) VALUES (?, ?, ?, ?)";
+	@Query("""
+	SELECT exists (SELECT u FROM User u WHERE u.loginId = ?1)
+""")
+	Boolean existsByLoginIdJPQL(String loginId);
 
-		jdbcTemplate.update(sql, user.getLoginId(), user.getPassword(), user.getName(), user.getBirthday());
-	}
+	Page<User> findAllByNameContaining(String name, Pageable pageable);
 }
